c7da0efdb0489b6735682236ff116128d45a3d42
Thiago <gianninimail@gmail.com>
2020-04-09 20:31:17 -0300

    Adição do script de criação da tabela pessoa

---
+++ create_table_pessoa.sql
@@ -0,0 +1,6 @@
+SELECT * FROM aulaweb.pessoa;CREATE TABLE `pessoa` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
